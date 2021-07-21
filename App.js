16e9f33805d94ecc938331670a71d9088785e66b
Luis Rangel <night@Luiss-MacBook-Pro.local>
2021-07-21 16:17:25 -0700

    updated profile, friends, and home screen files

--- App.js
+++ App.js
@@ -1,37 +1,3 @@
-// import React from "react";
-// import { StyleSheet } from "react-native";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
-// import ChatScreen from "./screens/ChatScreen";
-// import HomeScreen from "./screens/HomeScreen";
-
-// const Stack = createStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Home">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen name="Chat" component={ChatScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//   },
-//   item: {
-//     padding: 10,
-//     fontSize: 18,
-//     height: 44,
-//   },
-// });
-
-// export default App;
-
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useCallback, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
@@ -42,53 +8,44 @@ import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import firebase from "@firebase/app";
-
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 const Stack = createStackNavigator();
-
 function App() {
-
   const [isSignedIn, setIsSignedIn] = useState(
-    firebase.auth().currentUser ? true: false
+    firebase.auth().currentUser ? true : false
   );
-
   useEffect(() => {
     return firebase.auth().onAuthStateChanged((user) => {
-      setIsSignedIn(user ? true: false);
+      setIsSignedIn(user ? true : false);
     });
   }, []);
-
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Tabs">
-          
-          {isSignedIn ? (
-            <>
-              <Stack.Screen name="Tabs" component={BottomTabNavigator} />
-              <Stack.Screen name="Chat" component={ChatScreen} />
-            </>
-          ) : (
-            <>
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Signup" component={SignupScreen} />
-            </>
-          ) }
-          
-          {/* <>
-            <Stack.Screen name="Tabs" component={BottomTabNavigator} />
-            <Stack.Screen name="Chat" component={ChatScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Signup" component={SignupScreen} />
-          </> */}
-
-
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-      </NavigationContainer>
-    </View>
+    <ActionSheetProvider>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Tabs">
+            {isSignedIn ? (
+              <>
+                <Stack.Screen name="Tabs" component={BottomTabNavigator} />
+                <Stack.Screen name="Chat" component={ChatScreen} />
+              </>
+            ) : (
+              <>
+                <Stack.Screen
+                  name="Login"
+                  component={LoginScreen}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen name="Signup" component={SignupScreen} />
+              </>
+            )}
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </NavigationContainer>
+      </View>
+    </ActionSheetProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -100,5 +57,4 @@ const styles = StyleSheet.create({
     height: 44,
   },
 });
-
 export default App;
